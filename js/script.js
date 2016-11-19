af49da7668be4527b2e4830eabcd12fc641d105c
Donovan McRae <d_mcrae@ymail.com>
2016-11-19 12:08:16 -0500

    weekly site update; posting headliner notice

--- js/script.js
+++ js/script.js
@@ -2,8 +2,15 @@ $(document).ready(function(){
 	console.log('working');
 
 	$('.ep1-sc-ul').hide();
+	$('.ep2-sc-ul').hide();
 
 	$('#ep1sc').click(function(){
 		$('.ep1-sc-ul').toggle('1500');
-	})
+	});
+
+	$('#ep2sc').click(function(){
+		$('.ep2-sc-ul').toggle('1500');
+	});
+
+
 });

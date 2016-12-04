581345215385e0a664bb16960981136c108ffcf0
Donovan McRae <d_mcrae@ymail.com>
2016-12-04 18:01:35 -0500

    ep5 update

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
 	$('.ep1-sc-ul').hide();
 	$('.ep2-sc-ul').hide();
 	$('.ep3-sc-ul').hide();
+	$('.ep4-sc-ul').hide();
+
 
 	$('#ep1sc').click(function(){
 		$('.ep1-sc-ul').toggle('1500');
@@ -17,5 +19,8 @@ $(document).ready(function(){
 		$('.ep3-sc-ul').toggle('1500');
 	});
 
+	$('#ep4sc').click(function(){
+		$('.ep4-sc-ul').toggle('1500');
+	});
 
 });
